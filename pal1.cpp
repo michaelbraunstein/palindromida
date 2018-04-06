6793ee0cd0cf55fc1a5775f21c225aa0f6e1761b
Michael Braunstein <michaelbraunstein212@gmail.com>
2018-04-06 17:01:16 -0400

    Add files via upload

--- pal1.cpp
+++ pal1.cpp
@@ -29,21 +29,23 @@ void main() {
 	//int i;
 
 	cout << "Enter your palindrome string"; {
-		cin.getline(p1[i]); //, sizeof(p1[i], "\n")
+		cin.getline p1[i]; //, sizeof(p1[i], "\n")
 	}
 
 	cout << p1 << endl;
 
-	if (check) p1){
-	cout << p1 << " is a palindrome";
-	else
+	if (check(p1)) {
+		cout << p1 << " is a palindrome";
+	}
+	else {
 		cout << p1 << "is not a palindrome";
 	}
+}
 
 
 
 
-	void punct(p1[]) {
+void punct(p1[]) {
 
 		int i;
 		char local[66];
@@ -57,38 +59,34 @@ void main() {
 	}
 
 	//remove capitalization,   
-	void capt(p1[]) {
-		char lcl[66]
-			int i;
-		p1.size(p1.length()) {
-			for (i = 0, i < p1.length(); i++);
-			p1.at(i) = tolower(p1.at(i));
-		}
-		//p2.resize(p1.length);
+void capt(p1[]) {
+	char lcl[66]
+		int i;
+	p1.size(p1.length()) {
+		for (i = 0, i < p1.length(); i++);
+		p1.at(i) = tolower(p1.at(i));
 	}
+		//p2.resize(p1.length);
 
-	cout << p1[] << endl;
+
+		cout << p1[] << endl;
+	}
 
 	//palindrome check return type bool
 
 	bool check{
-			bool return = true;
+			bool rtrn = true;
 	for (int i = 0; i < p1.length / 2; i++) {
-		if (p1[i] != p1[p1.length(1 + i)])
- 
-		{
-			return = false;
+		if (p1[i] != p1[p1.length(1 + i)]) {
+			rtrn = false;
 			break;
 		}
-	
+	}
 	}
 
 
-	}
 
 
 
-
-}
 
 
