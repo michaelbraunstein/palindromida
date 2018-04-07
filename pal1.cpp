aa63546866b045ae805f4130d428bc1a6483c400
Michael Braunstein <michaelbraunstein212@gmail.com>
2018-04-06 23:02:53 -0400

    Add files via upload

--- pal1.cpp
+++ pal1.cpp
@@ -1,8 +1,8 @@
-/*Plaindrome exercise 2-5
+/*Palindrome exercise 2-5
 edition 1
-April 3, 20218
+April 3, 2018
 
-CISS 290 wohhleber */
+CISS 290 Wohhleber */
 
 #include<cctype>
 #include<cstring>
@@ -15,37 +15,29 @@ using namespace std;
 
 //prototypes 
 
-char[] punct(char[]);
-char[] capt(char[]);
-
-bool check(char[]);
-int i;
-char p1[66];
-
+void punct(char[]);
+void capt(char[]);
+bool ispal(char[]);
 
 void main() {
-	char *tokenptr;
-	//char p1[66];
-	//int i;
+	char p1[66];
+	cout << "Enter your palindrome string"; 
+	cin >> p1;
 
-	cout << "Enter your palindrome string"; {
-		cin.getline p1[i]; //, sizeof(p1[i], "\n")
-	}
+	cout << "String is: " << p1 << endl;
 
-	cout << p1 << endl;
 
-	if (check(p1)) {
+
+	if (ispal(p1)) 
 		cout << p1 << " is a palindrome";
-	}
-	else {
-		cout << p1 << "is not a palindrome";
-	}
+	else 
+		cout << p1 << " is not a palindrome";	
 }
 
 
 
 
-void punct(p1[]) {
+void punct(char p1) {
 
 		int i;
 		char local[66];
@@ -59,25 +51,29 @@ void punct(p1[]) {
 	}
 
 	//remove capitalization,   
-void capt(p1[]) {
-	char lcl[66]
+void capt(char p1) {
+	char lcl[66];
 		int i;
-	p1.size(p1.length()) {
-		for (i = 0, i < p1.length(); i++);
-		p1.at(i) = tolower(p1.at(i));
+		//p1.size=(char p1.length()); {
+		for (i = p1.length + 1, i =!'/n'; i--)
+		char p1.at(i) = tolower(p1.at(i));
 	}
 		//p2.resize(p1.length);
 
 
-		cout << p1[] << endl;
-	}
+	//	cout << p1 << endl;
+	
 
 	//palindrome check return type bool
 
-	bool check{
+bool ispal(char p1) {
 			bool rtrn = true;
-	for (int i = 0; i < p1.length / 2; i++) {
-		if (p1[i] != p1[p1.length(1 + i)]) {
+			for (i = p1.length + 1, i = !'\0'; i--) {
+				p2.append = char p1.at(i);
+			}
+
+	//for (int i = 0; i < p1.length / 2; i++) {
+		//if (p1[i] != p1[p1.length(1 + i)]) {
 			rtrn = false;
 			break;
 		}
