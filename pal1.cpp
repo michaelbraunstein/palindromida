8a71c2d89106d6460bdf4e512812af1e55d9bca2
Michael Braunstein <michaelbraunstein212@gmail.com>
2018-04-09 14:54:26 -0400

    Add files via upload

--- pal1.cpp
+++ pal1.cpp
@@ -4,15 +4,22 @@ April 3, 2018
 
 CISS 290 Wohhleber */
 
+#ifdef  _MSC_VER
+
+#define  _CRT_SECURE_NO_WARNINGS
+
+#endif
+
 #include<cctype>
 #include<cstring>
 #include<iostream>
 #include<iomanip>
 
-#include <stdio.h>
-#include <string.h>
+//#include <stdio.h>
+//#include <string.h>
 
 using namespace std;
+
  
  
 
@@ -24,29 +31,51 @@ bool ispal(char[]);
 
 void main() {
 	char p1[66];
-	cout << "Enter your palindrome string"; 
+	cout << "Enter your palindrome string: "; 
 	cin >> p1;
 
 	cout << "String is: " << p1 << endl;
+	cout << "debug 01 \n"; //debug************************************************************************
+	//system("pause");
+	cout << "debug 02 \n"; //debug************************************************************************
+	cout << "about to call punct and pass string with " << p1 << endl;
+	cout << "debug 03 \n"; //debug************************************************************************
+	//system("pause");
+	punct(p1);
+	cout << "punctuation removed" << p1;
+	//system("pause");
+	cout << "remove capitalizaion" << p1;
+	capt(p1);
+	cout << "return capt " << p1;
+	//system("pause");
+	if (ispal(p1)) {
+		cout << "if is pal";
 
-
-
-	if (ispal(p1)) 
 		cout << p1 << " is a palindrome";
-	else 
-		cout << p1 << " is not a palindrome";	
+	}
+
+	else {
+		cout << "else";
+		cout << p1 << " is not a palindrome";
+	}
+	system("pause");
 }
 
 
 
 
-void punct(char p1) {
+void punct(char p1[]) {
 
-		int i;
+		
 		char local[66];
-		char tokenptr = strtok(p1, " ,.!?;:+");
+		cout << "debug 10 \n"; //debug************************************************************************
+		cout << local;
+		char *tokenptr = strtok(p1, " ,.!?;:+");
+		cout << p1;
 		while (tokenptr != '\0') {
 			strcat(p1, tokenptr);
+			cout << p1;
+			cout << tokenptr;
 			cout << ' ';
 			tokenptr = strtok('\0', " ,.!?;:+");
 
@@ -54,12 +83,16 @@ void punct(char p1) {
 	}
 
 	//remove capitalization,   
-void capt(char p1) {
-	char lcl[66];
+void capt(char input_string[]) {
+	
 		int i;
+		cout << "debug 20 \n"; //debug************************************************************************
 		//p1.size=(char p1.length()); {
-		for (i = p1.length + 1, i =!'/n'; i--)
-		char p1.at(i) = tolower(p1.at(i));
+		for (i = strlen(input_string) - 1; i = 0; i--) {
+			input_string[i] = tolower(input_string[i]);
+		}
+
+		
 	}
 		//p2.resize(p1.length);
 
@@ -69,17 +102,24 @@ void capt(char p1) {
 
 	//palindrome check return type bool
 
-bool ispal(char p2, int i) {
-			bool rtrn = true;
-			for (i = p1.length + 1, i = !'\0'; i--); {
-				p2.append = char p1.at(i);
+bool ispal(char p1[]) {
+
+	cout << "debug 30 \n"; //debug************************************************************************
+	//char p2[63];
+//		int j = 0;
+//			bool rtrn = true;
+//			for (i = p1[].length + 1, i = !'\0'; i--); {
+//				p2[j] = p1[i];
+	return false;
 			}
 
 	//for (int i = 0; i < p1.length / 2; i++) {
 		//if (p1[i] != p1[p1.length(1 + i)]) {
-			rtrn = false;
+			//rtrn = false;
 			// break;
-		}
+			
+		
+		
 	
 
 
